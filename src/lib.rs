dda1a62213529e6c3e66aae7351c607171ebf09c
techyCoder81 <42820193+techyCoder81@users.noreply.github.com>
2024-01-30 23:12:44 -0500

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -110,7 +110,7 @@ pub enum SteamworksEvent {
     UserStatsReceived(steamworks::UserStatsReceived),
     UserStatsStored(steamworks::UserStatsStored),
     ValidateAuthTicketResponse(steamworks::ValidateAuthTicketResponse),
-    TicketForWebApiResponse(steamworks::TicketForWebApiResponse)
+    TicketForWebApiResponse(steamworks::TicketForWebApiResponse),
 }
 
 macro_rules! register_event_callbacks {
@@ -189,7 +189,7 @@ impl Plugin for SteamworksPlugin {
                         UserStatsReceived,
                         UserStatsStored,
                         ValidateAuthTicketResponse,
-                        TicketForWebApiResponse,
+                        TicketForWebApiResponse
                     ))
                     .insert_non_send_resource(single)
                     .add_event::<SteamworksEvent>()
